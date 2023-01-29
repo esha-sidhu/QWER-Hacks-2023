3cca961b9efe5dae34e4705cb093d8db8bebd554
lumingcao <lumingcao01@gmail.com>
2023-01-28 18:27:18 -0800

    Bring back changes

--- qwer-hacks-app/src/pages/settings.js
+++ qwer-hacks-app/src/pages/settings.js
@@ -10,15 +10,15 @@ function Settings () {
             <form id="form1">
 
             <h2>What best describes you?</h2>
-            <label for="var_string"><input type="radio" name="variable" id="var_string" />Newbie!</label>
-            <label for="var_join"><input type="radio" name="variable" id="var_join" />Beginner</label>
-            <label for="var_info"><input type="radio" name="variable" id="var_info" />Gardening Pro</label>
+            <label for="var_newbie"><input type="radio" name="role" id="var_newbie" />Newbie!</label>
+            <label for="var_beginner"><input type="radio" name="role" id="var_beginner" />Beginner</label>
+            <label for="var_pro"><input type="radio" name="role" id="var_pro" />Gardening Pro</label>
 
             <h2>What do you hope to grow?</h2>
-            <label for="var_string"><input type="radio" name="variable" id="var_string" />Veggies</label>
-            <label for="var_join"><input type="radio" name="variable" id="var_join" />Fruits</label>
-            <label for="var_info"><input type="radio" name="variable" id="var_info" />Flowers</label>
-            <label for="var_info"><input type="radio" name="variable" id="var_info" />Herbs</label>
+            <label for="plant_veggies"><input type="radio" name="plant" id="plant_veggies" />Veggies</label>
+            <label for="plant_fruits"><input type="radio" name="plant" id="plant_fruits" />Fruits</label>
+            <label for="plant_flowers"><input type="radio" name="plant" id="plant_flowers" />Flowers</label>
+            <label for="plant_herbs"><input type="radio" name="plant" id="plant_herbs" />Herbs</label>
 
             <h2>Where is your garden going to be?</h2>
             <label for="loc_in"><input type="radio" name="loc" id="loc_in" />Indoor</label>
@@ -30,9 +30,9 @@ function Settings () {
             <label for="pet_n"><input type="radio" name="pet" id="pet_n" />No</label>
 
             <h2>What type of soil do you have? (use your best estimate:) </h2>
-            <label for="var_string"><input type="radio" name="variable" id="var_string" />Gravelly</label>
-            <label for="var_join"><input type="radio" name="variable" id="var_join" />Sandy</label>
-            <label for="var_info"><input type="radio" name="variable" id="var_info" />Sticky</label>
+            <label for="soil_gravelly"><input type="radio" name="soil" id="soil_gravelly" />Gravelly</label>
+            <label for="soil_sandy"><input type="radio" name="soil" id="soil_sandy" />Sandy</label>
+            <label for="soil_sticky"><input type="radio" name="soil" id="soil_sticky" />Sticky</label>
 
 
             <button type="submit" value="Save">Save</button>
